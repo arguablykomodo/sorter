e52c1a4fbcefd6b9e02b7fff734a5bd29bde2f29
Komodo <noreply@komodo.ar>
2024-12-29 12:44:32 -0300

    feat: fresh start with sveltekit

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-/* @refresh reload */
-import { render } from "solid-js/web";
-
-import App from "./components/App.tsx";
-
-import "./style.css";
-
-const root = document.getElementById("root")!;
-render(() => <App />, root);
--- src/types.ts
+++
@@ -1,5 +0,0 @@
-export interface ItemData {
-  name: string;
-  link?: string;
-  image?: string;
-}
