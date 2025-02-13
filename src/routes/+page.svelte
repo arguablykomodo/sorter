09417465b35ecd44db7c0559716d8ae990a9dab2
Komodo <noreply@komodo.ar>
2025-02-13 20:52:00 -0300

    feat: shuffle before sorting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,6 +24,10 @@
   }
 
   function startSort() {
+    for (let i = items.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [items[i], items[j]] = [items[j], items[i]];
+    }
     sorting = true;
   }
 
