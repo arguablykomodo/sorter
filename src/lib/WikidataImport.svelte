5e928dd031c637b5699ed3515eea9a9b7152736c
Komodo <noreply@komodo.ar>
2025-07-07 19:24:07 -0300

    feat: disable wikidata presets

--- src/lib/WikidataImport.svelte
+++ src/lib/WikidataImport.svelte
@@ -4,23 +4,23 @@
 
   const { onImport }: { onImport: (items: ItemData[]) => void } = $props();
 
-  const presets = new Map([
-    [
-      "Country Flags",
-      `SELECT ?name ?link ?image
-WHERE {
-  ?item wdt:P31 wd:Q3624078 .
-  ?item wdt:P41 ?image .
-  ?item rdfs:label ?name filter (lang(?name) = "en")
-  ?link schema:about ?item .
-  ?link schema:inLanguage "en" .
-  ?link schema:isPartOf <https://en.wikipedia.org/> .
-}`,
-    ],
-  ]);
+//   const presets = new Map([
+//     [
+//       "Country Flags",
+//       `SELECT ?name ?link ?image
+// WHERE {
+//   ?item wdt:P31 wd:Q3624078 .
+//   ?item wdt:P41 ?image .
+//   ?item rdfs:label ?name filter (lang(?name) = "en")
+//   ?link schema:about ?item .
+//   ?link schema:inLanguage "en" .
+//   ?link schema:isPartOf <https://en.wikipedia.org/> .
+// }`,
+//     ],
+//   ]);
 
   let query: string | undefined = $state();
-  let selectedPreset: string | undefined = $state();
+  // let selectedPreset: string | undefined = $state();
   let promise = $state();
   let loading = $state(false);
 
@@ -44,18 +44,18 @@ WHERE {
       promise = fetchData().finally(() => (loading = false));
     }}
   >
-    <label>
-      Presets
-      <select
-        bind:value={selectedPreset}
-        onchange={() => (query = selectedPreset)}
-      >
-        <option value="" selected></option>
-        {#each presets as [name, value]}
-          <option {value}>{name}</option>
-        {/each}
-      </select>
-    </label>
+    <!-- <label> -->
+    <!--   Presets -->
+    <!--   <select -->
+    <!--     bind:value={selectedPreset} -->
+    <!--     onchange={() => (query = selectedPreset)} -->
+    <!--   > -->
+    <!--     <option value="" selected></option> -->
+    <!--     {#each presets as [name, value]} -->
+    <!--       <option {value}>{name}</option> -->
+    <!--     {/each} -->
+    <!--   </select> -->
+    <!-- </label> -->
     <label>
       SPARQL Query
       <textarea bind:value={query} placeholder="Pick a preset" required>
